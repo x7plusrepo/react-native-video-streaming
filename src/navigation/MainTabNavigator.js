e73101b3c3d4c90aafb27d77dc1eb38d3f8f474f
universegalaxy1112 <54145818+universegalaxy1112@users.noreply.github.com>
2021-06-15 10:42:05 +0800

    a bit update

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
-import {Image, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { Image, StyleSheet } from 'react-native';
 
-import {Constants, Global, GStyle, Helper, RestAPI,} from '../utils/Global';
+import { Constants, Global, GStyle, Helper, RestAPI } from '../utils/Global';
 import PlayTabScreen from '../screens/tab_play/PlayTabScreen';
 import TopUsersScreen from '../screens/tab_top/TopUsersScreen';
 import HomeMainScreen from '../screens/tab_home/HomeMainScreen';
 import BrowseRooms from '../screens/tab_liveStream/BrowseRooms';
 import ProfileMainScreen from '../screens/tab_profile/ProfileMainScreen';
 
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import {connect} from 'react-redux';
-import {setUnreadCount} from '../redux/message/actions';
+import { connect } from 'react-redux';
+import { setUnreadCount } from '../redux/message/actions';
 import avatars from '../assets/avatars';
-import {setMyUserAction} from '../redux/me/actions';
+import { setMyUserAction } from '../redux/me/actions';
 import ChatStreamSocketManager from '../utils/Message/SocketManager';
 
 const ic_tab_play = require('../assets/images/Icons/ic_tab_play.png');
@@ -67,11 +67,11 @@ class MainTabNavigator extends Component {
 
     showForcePageLoader(true);
     this.props.navigation.navigate('message');
-
   };
 
   render() {
     const { curTabName } = this.state;
+    const user = this.props.user;
 
     return (
       <Tab.Navigator
@@ -134,7 +134,7 @@ class MainTabNavigator extends Component {
           }}
           listeners={({ navigation, route }) => ({
             tabPress: (e) => {
-              if (!global.me) {
+              if (!user) {
                 e.preventDefault();
                 this.props.navigation.navigate('signin');
               }
@@ -150,7 +150,7 @@ class MainTabNavigator extends Component {
               const randomNumber = Math.floor(Math.random() * avatars.length);
               const randomImageUrl = avatars[randomNumber];
 
-              const avatarImage = { uri: global?.me?.photo ?? randomImageUrl };
+              const avatarImage = { uri: user?.photo ?? randomImageUrl };
               return (
                 <Image
                   source={avatarImage}
@@ -161,7 +161,7 @@ class MainTabNavigator extends Component {
           }}
           listeners={({ navigation, route }) => ({
             tabPress: (e) => {
-              if (!global.me) {
+              if (!user) {
                 e.preventDefault();
                 this.props.navigation.navigate('signin');
               }
@@ -173,9 +173,10 @@ class MainTabNavigator extends Component {
   }
 }
 
-export default connect((state) => ({}), { setUnreadCount, setMyUserAction })(
-  MainTabNavigator,
-);
+export default connect((state) => ({ user: state.me?.user }), {
+  setUnreadCount,
+  setMyUserAction,
+})(MainTabNavigator);
 
 const styles = StyleSheet.create({
   tabIconImage: {
